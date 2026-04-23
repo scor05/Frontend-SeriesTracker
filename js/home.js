0ada6fc4f683e508aaef37b4f9f6d49f07c42494
Santiago Cordero <santicordero28@gmail.com>
2026-04-23 01:21:43 -0600

    Primera versión funcional de pantalla principal

--- js/home.js
+++ js/home.js
@@ -3,21 +3,42 @@
 
 window.addEventListener('load', async () => {
     // resetear el input del topbar porque por alguna razón nunca se reseteaba
-    document.querySelector(".topbar-search").value = "";
+    document.querySelector(".topbar-search").value = ""
 
     // load inicial
-    const resp = await fetch("http://localhost:8080/series");
-    const series = await resp.json();
+    const resp = await fetch("http://localhost:42069/series")
+    const series = await resp.json()
 
     const table = document.querySelector(".content-table")
     for (const s of series) {
-        const tr = document.createElement("tr");
-        const cover = document.createElement("td").appendChild(document.createElement("img").src(s.img_src));
-        const text_desc = document.createElement("td").appendChild(document.createElement("p").append(s.name, document.createElement("br"), s.description));
-        const current_episode = document.createElement("td").appendChild(document.createElement("p").appendChild(s.current_episode))
-        const total_episodes = document.createElement("td").appendChild(document.createElement("p").appendChild(s.total_episodes))
-        const rating = document.createElement("td").appendChild(document.createElement("p").appendChild("Working on it"))
-        tr.appendChild(cover, text_desc, current_episode, total_episodes, rating)
+        const tr = document.createElement("tr")
+
+        const cover = document.createElement("td")
+        const img = document.createElement("img")
+        img.setAttribute("src", s.img_src)
+        cover.appendChild(img)
+
+        const text_desc = document.createElement("td")
+        const text_descElement = document.createElement("p")
+        text_descElement.classList.add("series-description")
+        text_descElement.append(s.name, document.createElement("br"), document.createElement("br"), s.description)
+        text_desc.appendChild(text_descElement)
+        const current_episode = document.createElement("td")
+        const current_episodeElement = document.createElement("p")
+        current_episodeElement.textContent = s.current_episode
+        current_episode.appendChild(current_episodeElement)
+
+        const total_episodes = document.createElement("td")
+        const total_episodesElement = document.createElement("p")
+        total_episodesElement.textContent = s.total_episodes
+        total_episodes.appendChild(total_episodesElement)
+
+        const rating = document.createElement("td")
+        const ratingElement = document.createElement("p")
+        ratingElement.textContent = "Working on it"
+        rating.appendChild(ratingElement)
+
+        tr.append(cover, text_desc, current_episode, total_episodes, rating)
         table.appendChild(tr)
     }
 
